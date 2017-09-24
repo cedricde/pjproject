e6a94598cc804ee9a1d54be0ffac44ef486af7bd
cedricde <cedricde@users.noreply.github.com>
2017-09-24 08:09:15 +0200

    Merge latest code from libsrtp 2.1.0

--- third_party/srtp/srtp/srtp.c
+++ third_party/srtp/srtp/srtp.c
@@ -81,13 +81,13 @@ srtp_debug_module_t mod_srtp = {
 
 static srtp_err_status_t
 srtp_validate_rtp_header(void *rtp_hdr, int *pkt_octet_len) {
-  srtp_hdr_t *hdr = (srtp_hdr_t *)rtp_hdr;
-  int rtp_header_len = octets_in_rtp_header + 4 * hdr->cc;
-
   if (*pkt_octet_len < octets_in_rtp_header)
     return srtp_err_status_bad_param;
 
+  srtp_hdr_t *hdr = (srtp_hdr_t *)rtp_hdr;
+
   /* Check RTP header length */
+  int rtp_header_len = octets_in_rtp_header + 4 * hdr->cc;
   if (hdr->x == 1)
     rtp_header_len += octets_in_rtp_extn_hdr;
 
@@ -701,7 +701,6 @@ typedef struct {
 static srtp_err_status_t srtp_kdf_init(srtp_kdf_t *kdf, const uint8_t *key, int key_len)
 {
     srtp_cipher_type_id_t cipher_id;
-    srtp_err_status_t stat;
     switch (key_len) {
     case SRTP_AES_ICM_256_KEY_LEN_WSALT:
         cipher_id = SRTP_AES_ICM_256;
@@ -717,6 +716,7 @@ static srtp_err_status_t srtp_kdf_init(srtp_kdf_t *kdf, const uint8_t *key, int 
         break;
     }
 
+    srtp_err_status_t stat;
     stat = srtp_crypto_kernel_alloc_cipher(cipher_id, &kdf->cipher, key_len, 0);
     if (stat) return stat;
 
